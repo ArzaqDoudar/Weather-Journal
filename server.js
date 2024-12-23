8a2d4f24ee8dae3950595c47c119d3b524b47668
arzaqziad <arzaqziad20@gmail.com>
2024-12-23 23:39:41 +0200

    commit the project

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
+let projectData = [];
+const port = 8000;
 // Require Express to run server and routes
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express()
 
 // Start up an instance of app
 
@@ -11,9 +16,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors())
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
+
+app.post('/addData', (req, res) => {
+    let data = req.body;
+    projectData.push(data)
+    console.log(projectData);
+    res.status(200).send('success')
+});
+
+app.get('/getData', (req, res) => {
+    console.log("Get")
+    res.send(projectData);
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,71 @@
-/* Global Variables */
+/* Global Variables  */
+
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather?q='
+const apiKey = '&APPID=eee1f1d1723341960bd881d4bcf95789&units=imperial'
+
+let dataObject = []
+
+const generateBtn = document.getElementById('generate');
+const zipInput = document.getElementById('zip')
+const feelingsInput = document.getElementById('feelings')
+const dateDiv = document.getElementById('date')
+const tempDiv = document.getElementById('temp')
+const contentDiv = document.getElementById('content')
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+
+const performAction = async () => {
+    const zipValue = zipInput.value;
+    const feelings = feelingsInput.value
+
+    if (zipValue && feelings) {
+        await postData(baseURL, zipValue, apiKey, feelings)
+        await getData()
+
+    }
+
+}
+//event 
+const postData = async (baseURL, zip, key, feeling) => {
+    const res = await fetch(baseURL + zip + key)
+    try {
+        const data = await res.json();
+        const newEntry = {
+            temperature: data.main.temp,
+            date: newDate,
+            feeling: feeling
+        }
+        const response = await fetch("http://localhost:8000/addData", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newEntry)
+        })
+        return response;
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+const getData = async () => {
+    const response = await fetch("/getData")
+    try {
+        const data = await response.json();
+        console.log('response from get data === ', typeof (data));
+        console.log('response from get data === ', data);
+        updateUI(data)
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+const updateUI = (data) => {
+    console.log('data', data);
+    dateDiv.innerHTML = data[0].date
+    tempDiv.innerHTML = data[0].temperature
+    contentDiv.innerHTML = data[0].feeling
+}
+generateBtn.addEventListener('click', performAction)
